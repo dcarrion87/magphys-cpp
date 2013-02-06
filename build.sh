fc5cdc011ef457d18c2e451e15366cb96cb00592
Daniel Carrion <dcarrion87@gmail.com>
2013-02-06 21:26:24 +1100

    Modify build script

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gfortran -g -O3 -ffixed-line-length-none -o fit_sed_fort fit_sed_skynet.f > /dev/null 2>&1
-g++ -g -O3 -fsingle-precision-constant -o fit_sed_cpp fit_sed_skynet.cpp > /dev/null 2>&1
+gfortran -O3 -ffixed-line-length-none -o fit_sed_fort fit_sed_skynet.f 
+g++ -O3 -fsingle-precision-constant -o fit_sed_cpp fit_sed_skynet.cpp
